43cd619b8f89b9de86813b89299ce18e8b00603b
David Daniel <davidedaniel@gmail.com>
2015-12-21 17:04:33 -0800

    calc portion working

--- timeScript.js
+++ timeScript.js
@@ -1,5 +1,5 @@
 
-var startTime = "09:00";
+// var startTime = "09:00";
 
 // some node points in the DOM
 var nodeStartPoint = '#B22_1_2';
@@ -7,78 +7,93 @@ var nodeStopPoint = '#B24_1_2';
 var fullHourNode = '#B26_1_2';
 
 var firstInput = document.querySelector('#B22_1_2');
-var startingTab = firstInput.closest('.x1u');
+var startingTab = firstInput.closest('.x1u').parentElement.closest('.x1u');
 var threeInputs = firstInput.closest('.x1u').getElementsByTagName('input');
 
 // calculate func factory
 
-function calculate(node,a,b,c) {
+function calculate(node) {
   return {
     equals: function() {
       return node
     },
-    _thisNode: function() {
-      node = node.closest('.x1u');
-      return this;
+    _node: function() {
+      return node;
     },
-    startInput: function() {
-      a = node.getElementsByTagName('input')[0]
-      a.value = moment().set({'hour':9,'minute':00}).format("H:mm")
+    startInput: function(custom) {
+      a = node.getElementsByTagName('input')[0];
+      if (custom) {
+        a.value = custom;
+        return a;
+      }
+      a.value = '09:00'
       return a;
     },
     stopInput: function() {
-      b = node.getElementsByTagName('input')[1]
-      b.value = moment().add(this.valInput)
+      b = node.getElementsByTagName('input')[1];
+
       return b;
     },
     valInput: function() {
-      c = node.getElementsByTagName('input')[2]
-      return c;
+      c = node.getElementsByTagName('input')[2];
+      return c.value;
+    },
+    switchTab: function(){
+      node = node.nextSibling;
+      return this
     },
     switchTime: function(){
 
+    },
+    calcStopTime: function(){
+      a = this.startInput().value;
+      b = this.stopInput();
+      c = this.valInput();
+      aMoment = new moment(a,'HH:mm');
+      var stopTime = aMoment.add('hours', c).format('HH:mm')
+      b.value = stopTime;
+      return stopTime;
     },
     render: function(){
 
     }
   }
 }
-
-function findBucket(node) {
-  return node.closest('.x1u');
-}
-
-function getBucketInputs(node) {
-  return node.getElementsByTagName('input');
-}
-
-function concatIt(num) {
-  var nodeId = '#B' + num + '_1_2';
-  return nodeId
-}
-
-function setFirstNode(id) {
-  var startingPoint = document.querySelector(id);
-  startingPoint.
-}
-
-function calculate(node) {
-  var hours = document.querySelector('#B26_1_2');
-}
-
-
-function dayNodeCalculator(a) {
-  var stopPoint = a + 2;
-}
-
-function nextBucket(b) {
-  var nextBucket = b + 1;
-}
-
-function nextDay(c) {
-  var nextDay = c + 1;
-}
-
-function nodeCalculator(22, 1, 2) {
-  day
-}
+//
+// function findBucket(node) {
+//   return node.closest('.x1u');
+// }
+//
+// function getBucketInputs(node) {
+//   return node.getElementsByTagName('input');
+// }
+//
+// function concatIt(num) {
+//   var nodeId = '#B' + num + '_1_2';
+//   return nodeId
+// }
+//
+// function setFirstNode(id) {
+//   var startingPoint = document.querySelector(id);
+// }
+//
+// function calculate(node) {
+//   var hours = document.querySelector('#B26_1_2');
+// }
+//
+//
+// function dayNodeCalculator(a) {
+//   var stopPoint = a + 2;
+// }
+//
+// function nextBucket(b) {
+//   var nextBucket = b + 1;
+// }
+//
+// function nextDay(c) {
+//   var nextDay = c + 1;
+// }
+//
+// function nodeCalculator(22, 1, 2) {
+//   // day
+// }
